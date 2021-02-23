afaed264c2a80f36b8b419822cec53253aad5a6d
Rusttm <rust_mazh@mail.ru>
2021-02-23 21:00:50 +0300

    Update scrapy_lib.py

--- scrapy_lib.py
+++ scrapy_lib.py
@@ -1,6 +1,7 @@
 import scrapy
 import google_books
 
+
 class ForestSpider(scrapy.Spider):
     name = "forest_spider"
     start_urls = ['https://www.for-est.ru/catalog/skoby/obivochnye/']
@@ -8,13 +9,13 @@ class ForestSpider(scrapy.Spider):
     def parse(self, response):
         names = response.xpath('//div[@class = "description"]/div[@class = "desc_name"]/a/text()').extract()
         prices = response.xpath('//div[@class = "price"]/span/text()').extract()
-        #print([names, prices])
         print(google_books.send_to_forest_sheet(names, prices))
         next_page = response.xpath('//a[@class = "arrow right"]/@href').extract_first()
         print('результат запроса -', next_page)
         if next_page:
+            """run new page search"""
+            print('moving to next page', next_page)
             yield scrapy.Request(
                 response.urljoin(next_page),
                 callback=self.parse
             )
-
