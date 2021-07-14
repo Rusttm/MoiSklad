742e9cec3dcd75c9337148e31086eb409bace18b
Rusttm <rust_mazh@mail.ru>
2021-07-14 12:26:47 +0300

    spare parts sales

---
+++ sale_spare_parts.py
@@ -0,0 +1,156 @@
+""" discont on spareparts """
+import configparser
+import requests
+import os
+from datetime import date
+import json
+import pandas as pd
+import xlsxwriter
+
+
+
+try:
+    # get data from in file
+    conf = configparser.ConfigParser()
+    #conf.read('config.ini')
+    conf.read(os.path.join(os.path.dirname(__file__), 'config/config.ini'))
+    URL = conf['MoiSklad']['URL']
+    URL_TOKEN = conf['MoiSklad']['URL_TOKEN']
+    url_otgruzka_list = conf['MoiSklad']['url_otgruzka_list']
+    url_money = conf['MoiSklad']['url_money']
+    my_access_token = conf['MoiSklad']['access_token']
+    header_for_token_auth = {'Authorization': 'Bearer %s' % my_access_token}
+    url_customers = conf['MoiSklad']['url_customers']
+    url_sales_list = conf['MoiSklad']['url_sales_list']
+    url_nsk_group = conf['TAGS']['Nsk']
+    url_prod_list = conf['MoiSklad']['url_prod_list']
+
+
+except IndexError:
+    print('Error, cant read .ini file', Exception)
+
+agent_group_href = {'Saratov' : 'https://online.moysklad.ru/api/remap/1.2/entity/group/735e1535-570a-11eb-0a80-04f2002a960a'}
+
+class spare_parts_check():
+    def __init__(self):
+        self.demands_dict = {}
+        self.clients_dict = {}
+        self.products_dict = {}
+        self.agents_sold_spare_parts = {}
+
+        self.work_with_demand_file()
+        self.work_with_clients_file()
+        self.work_with_prod_file()
+
+
+    def get_positions_list(self):
+        """get prod list"""
+        x =[]
+        y = []
+        try:
+            for tag_positions, [tag_demand, tag_customer] in self.demands_dict.items():
+                positions_req = requests.get(url=f"{tag_positions}", headers=header_for_token_auth)
+                for position in positions_req.json()['rows']:
+                    prod_tag = position['assortment']['meta']['href']
+                    prod_discount = position['discount']
+                    if (prod_discount>0) and (prod_tag in self.products_dict.keys()):
+                        customer_name = self.clients_dict[tag_customer]
+                        y = self.agents_sold_spare_parts.get(customer_name, [])
+                        if tag_demand not in y:
+                            x = y + [tag_demand]
+                        else:
+                            x = y
+                        self.agents_sold_spare_parts[customer_name] = x
+            print(self.agents_sold_spare_parts.keys())
+            print(self.agents_sold_spare_parts)
+            self.write_to_file()
+
+
+        except IndexError:
+            print('Cant read sales data', Exception)
+
+    def write_to_file(self):
+        result_dict = self.agents_sold_spare_parts
+        with xlsxwriter.Workbook('spare_parts_discount.xlsx') as workbook:
+            r = 0
+            # Add worksheet
+            worksheet = workbook.add_worksheet()
+
+            # Write headers
+            for head, values in result_dict.items():
+                worksheet.write(0, r, head)
+                # Write list data
+                for i, value in enumerate(values, start=1):
+                    worksheet.write(i, r, value)
+                r += 1
+
+
+    def get_demands_list(self):
+        """get demands list"""
+        today = date.today()
+        today_date = str(today.strftime("%d.%m.%y_%H:%M"))
+        today_date_req = str(today.strftime("%Y-%m-%d"))
+        failed_sales_list = []
+        try:
+            sales_req = requests.get(url=f"{url_otgruzka_list}", headers=header_for_token_auth)
+            with open('demand_req_list.json', 'w') as ff:
+                json.dump(sales_req.json(), ff, ensure_ascii=False)
+        except IndexError:
+            print('Cant read sales data', Exception)
+
+    def get_clients_list(self):
+        """get clients list"""
+        try:
+            clients_req = requests.get(url=f"{url_customers}", headers=header_for_token_auth)
+            with open('clients_req_list.json', 'w') as ff:
+                json.dump(clients_req.json(), ff, ensure_ascii=False)
+        except IndexError:
+            print('Cant read sales data', Exception)
+
+    def get_prod_list(self):
+        """get clients list"""
+        try:
+            clients_req = requests.get(url=f"{url_prod_list}", headers=header_for_token_auth)
+            with open('prod_req_list.json', 'w') as ff:
+                json.dump(clients_req.json(), ff, ensure_ascii=False)
+        except IndexError:
+            print('Cant read sales data', Exception)
+
+    def work_with_demand_file(self):
+        with open('demand_req_list.json', 'r') as json_data:
+            d = json.load(json_data)
+            for demand in d['rows']:
+                tag_positions = demand['positions']['meta']['href']
+                tag_group = demand['group']['meta']['href']
+                tag_customer = demand['agent']['meta']['href']
+                tag_demand = demand['meta']['uuidHref']
+                if tag_group == url_nsk_group:
+                    self.demands_dict[tag_positions] = [tag_demand, tag_customer]
+
+    def work_with_clients_file(self):
+        with open('clients_req_list.json', 'r') as json_data:
+            customers = json.load(json_data)
+            for customer in customers['rows']:
+                tag_customer = customer['meta']['href']
+                name_customer = customer['name']
+                self.clients_dict[tag_customer] = name_customer
+
+
+    def work_with_prod_file(self):
+        with open('prod_req_list.json', 'r') as json_data:
+            products = json.load(json_data)
+            for prod in products['rows']:
+                tag_prod = prod['meta']['href']
+                name_prod = prod['name']
+                full_group_name = prod['pathName']
+                try:
+                    prod_group_name = full_group_name.split('/')[1]
+                except:
+                    prod_group_name = 'unknown'
+                if prod_group_name == 'Запасные части':
+                    self.products_dict[tag_prod] = name_prod
+
+
+
+x = spare_parts_check()
+print(x.get_positions_list())
--- trainings.py
+++ trainings.py
@@ -1,7 +1,9 @@
+# -*- coding: cp1251 -*-
 import urllib3
 from bs4 import BeautifulSoup
-
+import pandas as pd
 import pars_sites
+import xlsxwriter
 
 
 def test1():
@@ -15,5 +17,24 @@ def test1():
     quotes_names_block = soup.find_all('div', class_='catalog-model-name catalog-list-tile-name')
     print(len(quotes_names_block), response.status, response.data, response.headers)
 
+x = {'ÎÎÎ "ÈÍÒÅĞ-ÒĞÅÉÄ"': ['https://online.moysklad.ru/app/#demand/edit?id=2174c35f-cd9d-11eb-0a80-000a0030abd9', 'https://online.moysklad.ru/app/#demand/edit?id=2437c630-ad82-11eb-0a80-014f00081aee', 'https://online.moysklad.ru/app/#demand/edit?id=76781a13-cda0-11eb-0a80-051e00311358', 'https://online.moysklad.ru/app/#demand/edit?id=c056a4c0-7021-11eb-0a80-052c00043846', 'https://online.moysklad.ru/app/#demand/edit?id=d69f1f51-a319-11eb-0a80-06850002042b', 'https://online.moysklad.ru/app/#demand/edit?id=edb8e8ed-8604-11eb-0a80-0470003c8aa1'], 'ÎÎÎ "ÌÒ-ÑÈÁÈĞÜ"': ['https://online.moysklad.ru/app/#demand/edit?id=2d1c1eb3-9c3a-11eb-0a80-008800097d4a'], 'ÎÎÎ ÏÒÊ "ÀÑÊÎ"': ['https://online.moysklad.ru/app/#demand/edit?id=7280e678-6cd7-11eb-0a80-07710001f8fd', 'https://online.moysklad.ru/app/#demand/edit?id=7c16c19d-9c38-11eb-0a80-032000093dee'], 'ÎÎÎ "ÏÀËËÅÒ ÑÅĞÂÈÑ"': ['https://online.moysklad.ru/app/#demand/edit?id=98b21e1c-7805-11eb-0a80-06660003c2b4'], 'ÎÎÎ "ÊĞÀÑÄÈÂÀÍ"': ['https://online.moysklad.ru/app/#demand/edit?id=d8f646e6-e2ef-11eb-0a80-04a400287d5c'], 'ÎÎÎ "İÃÈÄÀ-ÑÈÁÈĞÜ"': ['https://online.moysklad.ru/app/#demand/edit?id=eb620f65-9815-11eb-0a80-0d3100016f06']}
 
-print(pars_sites.parsing_serman_site())
+
+def write_df():
+    with xlsxwriter.Workbook('spare_parts_discount.xlsx') as workbook:
+        r=0
+        # Add worksheet
+        worksheet = workbook.add_worksheet()
+
+        # Write headers
+        for head, values in x.items():
+            worksheet.write(0, r, head)
+            # Write list data
+            for i, value in enumerate(values, start=1):
+                worksheet.write(i, r, value)
+            r += 1
+
+
+
+write_df()
+
