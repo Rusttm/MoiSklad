bddc6b282e33b30d430c7522abeacd5ff465767a
Rustam <52700794+Rusttm@users.noreply.github.com>
2024-02-14 02:04:50 +0300

    profit and balanses report module fin commit

--- MoiSkladPackage/MSControllers/MSGSControllerAsync.py
+++ MoiSkladPackage/MSControllers/MSGSControllerAsync.py
@@ -25,7 +25,7 @@ class MSGSControllerAsync(GSMSContAsync):
             print(msg)
         return balances_data
 
-    async def save_profit_gs_async(self, from_date, to_date, report_type="custom") -> pd.DataFrame:
+    async def save_profit_gs_custom_async(self, from_date, to_date, report_type="custom") -> pd.DataFrame:
         """ saves profit ms data to google spread"""
         profit_data = pd.DataFrame()
         try:
@@ -36,11 +36,42 @@ class MSGSControllerAsync(GSMSContAsync):
             msg = f"{__class__.__name__} saves balance data to spreadsheet. "
             self.logger.debug(msg)
         except Exception as e:
+            msg = f"{__class__.__name__} cant custom profit report to spreadsheet, Error: \n {e} "
+            self.logger.warning(msg)
+            print(msg)
+        return profit_data
+
+    async def save_profit_gs_daily_async(self, report_type="daily") -> pd.DataFrame:
+        """ saves profit ms data to google spread"""
+        profit_data = pd.DataFrame()
+        try:
+            from MoiSkladPackage.MSReports.MSReportProfitAsync import MSReportProfitAsync
+            connector = MSReportProfitAsync()
+            profit_data = await connector.get_daily_profit_report_async()
+            await self.save_data_ms_gs_async(profit_data, gs_tag="gs_profit", ws_id=539265374)
+            msg = f"{__class__.__name__} saves daily profit report to spreadsheet. "
+            self.logger.debug(msg)
+        except Exception as e:
             msg = f"{__class__.__name__} cant saves balance data to spreadsheet, Error: \n {e} "
             self.logger.warning(msg)
             print(msg)
         return profit_data
 
+    async def save_profit_gs_monthly_async(self, to_year: int, to_month: int) -> pd.DataFrame:
+        """ saves profit ms data to google spread"""
+        profit_data = pd.DataFrame()
+        try:
+            from MoiSkladPackage.MSReports.MSReportProfitAsync import MSReportProfitAsync
+            connector = MSReportProfitAsync()
+            profit_data = await connector.get_monthly_profit_report_async(to_year=to_year, to_month=to_month)
+            await self.save_data_ms_gs_async(profit_data, gs_tag="gs_profit", ws_id=539265374)
+            msg = f"{__class__.__name__} saves daily profit report to spreadsheet. "
+            self.logger.debug(msg)
+        except Exception as e:
+            msg = f"{__class__.__name__} cant saves balance data to spreadsheet, Error: \n {e} "
+            self.logger.warning(msg)
+            print(msg)
+        return profit_data
 
 
 
@@ -49,5 +80,7 @@ if __name__ == "__main__":
     # print(connect.get_stores_good_price())
     # print(connect.get_stores_dict())
     # print(asyncio.run(controller.save_balance_gs_async()))
-    print(asyncio.run(controller.save_profit_gs_async(from_date="2024-01-01", to_date="2024-01-09")))
+    # print(asyncio.run(controller.save_profit_gs_custom_async(from_date="2024-01-01", to_date="2024-01-09")))
+    # print(asyncio.run(controller.save_profit_gs_daily_async()))
+    print(asyncio.run(controller.save_profit_gs_monthly_async(to_year=2022, to_month=12)))
     controller.logger.debug("stock_all class initialized")
