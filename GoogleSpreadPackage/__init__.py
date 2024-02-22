410934aa1bfe9e3db1e09e3bef7aeb5600af47c5
Rustam <52700794+Rusttm@users.noreply.github.com>
2024-02-22 15:34:13 +0300

    remove new bot from old repository

--- GoogleSpreadPackage/__init__.py
+++
@@ -1,7 +0,0 @@
-import GoogleSpreadPackage
-from .GSPkgLogger import GSPkgLogger
-from .GSConnAsync import GSConnAsync
-from .GSMainClass import GSMainClass
-from .GSMSDataHandlerAsync import GSMSDataHandlerAsync
-
-
--- MoiSkladPackage/MSConnectors/__init__.py
+++
@@ -1,3 +0,0 @@
-from MoiSkladPackage.MSConnectors.MSMainClass import MSMainClass
-# from MSReadJsonAsync import MSReadJsonAsync
-# from MSSaveJsonAsync import MSSaveJsonAsync
